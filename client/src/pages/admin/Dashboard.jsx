1f5825f89b612ea3fb2f6afd0cb3696b5e29c4f8
Aman Ahamed <amanahamed04@gmail.com>
2026-02-01 15:10:45 +0530

    table component - dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -41,7 +41,7 @@ const Dashboard = () => {
           </div>
         </div>
 
-         <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all">
+        <div className="flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all">
           <img src={assets.dashboard_icon_3} />
           <div>
             <p className="text-xl font-semibold text-gray-600">
@@ -50,7 +50,45 @@ const Dashboard = () => {
             <p className="text-gray-400 font-light">Drafts</p>
           </div>
         </div>
+      </div>
 
+      <div>
+        <div className="flex items-center gap-3 m-4 mt-6 text-gray-600">
+          <img src={assets.dashboard_icon_4} />
+          <p>Latest Blogs</p>
+        </div>
+
+        <div className="relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+          <table className="w-full text-sm text-gray-900">
+            <thead className="text-xs text-gray-600 text-left uppercase">
+              <tr>
+                <th scope="col" className="px-2 py-4 xl:px-6">
+                  {" "}
+                  #{" "}
+                </th>
+                <th scope="col" className="px-2 py-4">
+                  {" "}
+                  Blog Title
+                </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden">
+                  {" "}
+                  Date
+                </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden">
+                  {" "}
+                  Status
+                </th>
+                <th scope="col" className="px-2 py-4">
+                  {" "}
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
