183737463520a6627c50e192fb62447dc31cb3bf
Aman Ahamed <amanahamed04@gmail.com>
2026-02-01 16:33:51 +0530

    add blog - images

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
 
 const AddBlog = () => {
-  return (
-    <div>
-      dd
-    </div>
-  )
-}
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubTitle] = useState("");
+  const [category, setCategory] = useState("Startup");
+  const [isPublished, setIsPublished] = useState(false);
 
-export default AddBlog
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll"
+    >
+      <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded">
+        <p>Upload Thumbnail</p>
+        <label htmlFor="image">
+          <img
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            className="mt-2 h-16 rounded cursor-pointer"
+          />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
+        </label>
+      </div>
+    </form>
+  );
+};
+
+export default AddBlog;
