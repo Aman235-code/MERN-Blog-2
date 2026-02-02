4d04c02f5b22c9eecba0c734c2e0f23410e1ba14
Aman Ahamed <amanahamed04@gmail.com>
2026-02-02 13:04:31 +0530

    database connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 const app = express();
+
+await connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -11,10 +14,10 @@ app.get("/", (req, res) => {
   res.send("APi is working");
 });
 
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server is running on Port " + PORT);
 });
 
-// export default app;
+export default app;
