ea865b90d39752d84318499550bc0b77de886095
Aman Ahamed <amanahamed04@gmail.com>
2026-02-02 12:38:14 +0530

    server setup

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("APi is working");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on Port " + PORT);
+});
+
+export default app;
