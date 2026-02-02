1be3941511a5bdf285d3e39a5a73e891e13e383f
Aman Ahamed <amanahamed04@gmail.com>
2026-02-02 12:53:52 +0530

    server app start

--- server/server.js
+++ server/server.js
@@ -4,17 +4,17 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("APi is working");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3006;
 
 app.listen(PORT, () => {
   console.log("Server is running on Port " + PORT);
 });
 
-export default app;
+// export default app;
