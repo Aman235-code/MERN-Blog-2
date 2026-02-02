1670cb17e5e6fc8ca9e5ea639a6a9d7ff53c88d4
Aman Ahamed <amanahamed04@gmail.com>
2026-02-02 13:18:25 +0530

    admin login

---
+++ server/controllers/adminController.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (
+      email != process.env.ADMIN_EMAIL ||
+      password != process.env.ADMIN_PASSWORD
+    ) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET);
+    return res.status(200).json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
