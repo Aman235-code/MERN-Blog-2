ebb3c4822ddf7acdb67cc5202f64bd8a546ad5a8
Aman Ahamed <amanahamed04@gmail.com>
2026-02-03 15:53:15 +0530

    comments page

--- client/src/components/admin/CommentTableItem.jsx
+++ client/src/components/admin/CommentTableItem.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const CommentTableItem = ({ comment, fetchComments }) => {
   const { blog, createdAt, _id } = comment;
 
   const BlogDate = new Date(createdAt);
+
+  const { axios } = useAppContext();
+
+  const approveComment = async () => {
+    try {
+      const { data } = await axios.post("/api/admin/approve-comment", {
+        id: _id,
+      });
+      data.success ? toast.success(data.message) : toast.error(data.message);
+      fetchComments();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const deleteComment = async () => {
+    try {
+      const confirm = window.confirm("Are you sure?");
+      if (!confirm) return;
+
+      const { data } = await axios.post("/api/admin/delete-comment", {
+        id: _id,
+      });
+      data.success ? toast.success(data.message) : toast.error(data.message);
+      fetchComments();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <tr className="order-y border-gray-300">
@@ -24,6 +55,7 @@ const CommentTableItem = ({ comment, fetchComments }) => {
           {!comment.isApproved ? (
             <img
               src={assets.tick_icon}
+              onClick={approveComment}
               className="w-5 hover:scale-110 transition-all cursor-pointer"
             />
           ) : (
@@ -32,6 +64,7 @@ const CommentTableItem = ({ comment, fetchComments }) => {
             </p>
           )}
           <img
+            onClick={deleteComment}
             src={assets.bin_icon}
             className="w-5 hover:scale-110 transition-all cursor-pointer"
           />
