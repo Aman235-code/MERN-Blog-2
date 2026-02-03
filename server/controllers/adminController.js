989c87ef517ea3854b1d205ad8978234fbd1b096
Aman Ahamed <amanahamed04@gmail.com>
2026-02-03 13:12:12 +0530

    admin comments controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import Blog from "../models/Blog.js";
+import Comment from "../models/Comment.js";
 
 export const adminLogin = async (req, res) => {
   try {
@@ -37,7 +38,24 @@ export const getAllBlogsAdmin = async (req, res) => {
       success: true,
       blogs,
     });
-    
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({}).populate("blog").sort({
+      createdAt: -1,
+    });
+
+    return res.status(200).json({
+      success: true,
+      comments,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
